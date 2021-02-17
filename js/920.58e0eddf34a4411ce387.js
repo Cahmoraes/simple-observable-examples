dc80646329aac2207a7ec3d1b80e6760059db066
Cahmoraes <ike.moraes93@gmail.com>
2021-02-17 19:12:11 -0300

    Updates

--- js/920.58e0eddf34a4411ce387.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkrevisao_10_02_2021=self.webpackChunkrevisao_10_02_2021||[]).push([[920],{920:n=>{function s(){return`<h2 class="subtitle">${this.title}</h2>\n<div class="container-fluid">\n  <div class="grid-12">\n    <img class="notfound" src="${this.notfoundImg}" alt="Page not found">\n  </div>\n</div>`}n.exports=function(n){return s.call(n,n)}}}]);
