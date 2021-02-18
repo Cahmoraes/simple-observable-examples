b60ca923c67eaf688d7fde7ebee4e9c2181698d7
Cahmoraes <ike.moraes93@gmail.com>
2021-02-18 20:48:04 -0300

    favicon changed

--- src/index.js
+++ src/index.js
@@ -7,14 +7,3 @@ import Router from './components/Router'
 const router = new Router().init()
 new Menu(router).init()
 new Main(router).init()
-
-const $observable = so.observable(1)
-const $pipe = $observable.pipe(v => v.toString())
-$observable(3)
-// const sub = $pipe.subscribe(console.log)
-// sub.dispose()
-$pipe(4)
-$pipe(6)
-
-console.log($observable())
-console.log($pipe())
